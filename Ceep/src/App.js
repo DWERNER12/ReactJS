ac2cdca42208755a936e2d11a3c129d2d3e7cfbf
DWERNER12 <davidwerner1995@gmail.com>
2022-01-16 21:03:01 -0300

    aula1

---
+++ Ceep/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import FormularioCadastro from './component/FormularioCadastro';
+import ListaDeNotas from './component/ListaDeNotas';
+
+function App() {
+  return (
+    <section>
+      <FormularioCadastro/>
+      <ListaDeNotas/>
+    </section>
+    
+  );
+}
+
+export default App;
