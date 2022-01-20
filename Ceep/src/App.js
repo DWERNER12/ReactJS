7ad7dff3acd3cf409b46a7a15122c8cb98a80b17
DWERNER12 <davidwerner1995@gmail.com>
2022-01-19 22:14:16 -0300

    aula2

--- Ceep/src/App.js
+++ Ceep/src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import './App.css';
-import FormularioCadastro from './component/FormularioCadastro';
-import ListaDeNotas from './component/ListaDeNotas';
-
-function App() {
-  return (
-    <section>
-      <FormularioCadastro/>
-      <ListaDeNotas/>
-    </section>
-    
-  );
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import "./assets/index.css";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notas:[]
+    }
+  }
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
+      </section>
+    );
+  }
 }
 
 export default App;
